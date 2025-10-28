780154f00294ec3989655c8dfc7ea4c209bbb519
pfrey <82400804+patfrey@users.noreply.github.com>
2025-10-28 20:57:03 +0100

    add basic tests for cli arg combinations

--- PaperMore.Tests/TestCli.cs
+++ PaperMore.Tests/TestCli.cs
@@ -4,6 +4,8 @@ namespace PaperMore.Tests;
 
 public class TestCli
 {
+    public record CliTestSet(string[] Args, CmdArgs? Result, int ReturnCode, string TestCase);
+
     private CmdParser _parser;
 
     [SetUp]
@@ -14,40 +16,106 @@ public class TestCli
 
     [Test]
     [TestCaseSource(nameof(GetSuccessArgTests))]
-    public void TestParseShouldSucceed((string[], CmdArgs, int) testData)
+    public void TestParseShouldSucceed(CliTestSet testData)
     {
         CmdArgs? cmdArgs = null;
-        int returnCode = _parser.TryParse(testData.Item1, result => cmdArgs = result);
-        
-        Assert.That(returnCode, Is.EqualTo(testData.Item3));
+        int returnCode = _parser.TryParse(testData.Args, result => cmdArgs = result);
+
+        Assert.That(returnCode, Is.EqualTo(testData.ReturnCode));
         Assert.That(cmdArgs, Is.Not.Null);
-        Assert.That(cmdArgs.Url, Is.EqualTo(testData.Item2.Url));
-        Assert.That(cmdArgs.Token, Is.EqualTo(testData.Item2.Token));
-        Assert.That(cmdArgs.Format, Is.EqualTo(testData.Item2.Format));
-        Assert.That(cmdArgs.BatchSize, Is.EqualTo(testData.Item2.BatchSize));
-        Assert.That(cmdArgs.BlankLines, Is.EqualTo(testData.Item2.BlankLines));
-    }
-    
-    [Test]
-    [TestCaseSource(nameof(GetFailArgsTests))]
-    public void TestParseShouldFail((string[], CmdArgs, int) testData)
-    {
-        CmdArgs? cmdArgs = null;
-        int returnCode = _parser.TryParse(testData.Item1, result => cmdArgs = result);
-        
-        Assert.That(returnCode, Is.EqualTo(testData.Item3));
-        Assert.That(cmdArgs, Is.Null);
-    }
-        
-    private static IEnumerable<(string[], CmdArgs, int)> GetSuccessArgTests()
-    {
-        yield return (new[] { "--url", "http://localhost:8080", "--token", "123456789", "--format", "pdf", "--output", "/home/test/index.pdf", "--blanklines", "25", "--batch-size", "100" }, new CmdArgs("http://localhost:8080", "123456789", FormatType.Pdf, "/home/test/index.pdf", 25, 100, null, null), 0);
-        yield return (new[] { "-u", "http://localhost:8080", "-t", "123456789", "-f", "pdf", "-o", "/home/test/index.pdf", "-b", "25", "-B", "100" }, new CmdArgs("http://localhost:8080", "123456789", FormatType.Pdf, "/home/test/index.pdf", 25, 100, null, null), 0);
-        
+        Assert.That(cmdArgs.Url, Is.EqualTo(testData.Result!.Url));
+        Assert.That(cmdArgs.Token, Is.EqualTo(testData.Result.Token));
+        Assert.That(cmdArgs.Format, Is.EqualTo(testData.Result.Format));
+        Assert.That(cmdArgs.BatchSize, Is.EqualTo(testData.Result.BatchSize));
+        Assert.That(cmdArgs.BlankLines, Is.EqualTo(testData.Result.BlankLines));
     }
 
-    private static IEnumerable<(string[], CmdArgs, int)> GetFailArgsTests()
+    [Test]
+    [TestCaseSource(nameof(GetFailArgsTests))]
+    public void TestParseShouldFail(CliTestSet testData)
     {
-        yield return (new[] { "-u", "http://localhost:8080", "-t", "123456789", "-f", "csv", "-o", "/home/test/index.pdf", "-b", "25", "-B", "100" }, new CmdArgs("http://localhost:8080", "123456789", FormatType.Csv, "/home/test/index.pdf", 25, 100, null, null), 1);
+        CmdArgs? cmdArgs = null;
+        int returnCode = _parser.TryParse(testData.Args, result => cmdArgs = result);
+
+        Assert.That(returnCode, Is.EqualTo(testData.ReturnCode), testData.TestCase);
+        Assert.That(cmdArgs, Is.Null, testData.TestCase);
+    }
+
+    private static IEnumerable<CliTestSet> GetSuccessArgTests()
+    {
+        yield return new CliTestSet(
+            [
+                "--url", "http://localhost:8080", "--token", "123456789", "--format", "pdf", "--path",
+                "/home/test/index.pdf", "--blanklines", "25", "--batch-size", "100"
+            ],
+            new CmdArgs("http://localhost:8080", "123456789", FormatType.Pdf, "/home/test/index.pdf", 25, 100, null,
+                null),
+            0,
+            "Long Params");
+        yield return new CliTestSet(
+            [
+                "-u", "http://localhost:8080", "-t", "123456789", "-f", "pdf", "-p", "/home/test/index.pdf", "-b", "25",
+                "-B", "100"
+            ],
+            new CmdArgs("http://localhost:8080", "123456789", FormatType.Pdf, "/home/test/index.pdf", 25, 100, null,
+                null),
+            0,
+            "Short Params");
+    }
+
+    private static IEnumerable<CliTestSet> GetFailArgsTests()
+    {
+        yield return new CliTestSet(
+            ["-t", "123456789", "-p", "/home/test/index.csv"],
+            null,
+            1,
+            "no url provided"
+        );
+        yield return new CliTestSet(
+            ["-u", "http://localhost:8080", "-p", "/home/test/index.csv"],
+            null,
+            1,
+            "no token provided");
+        yield return new CliTestSet(
+            ["-u", "http://localhost:8080", "-t", "123456789"],
+            null,
+            1,
+            "no path provided");
+        yield return new CliTestSet(
+            ["-u", "http://localhost:8080", "-t", "123456789", "-f", "csv", "-p", "/home/test/index.csv", "-b", "25"],
+            null,
+            1,
+            "--blank-lines parameter only in pdf"
+        );
+        yield return new CliTestSet(
+            ["-u", "http://localhost:8080", "-t", "123456789", "-f", "csv", "-p", "/home/test/index.csv", "-B", "-1"],
+            null,
+            1,
+            "--batch-size less than 0"
+        );
+        yield return new CliTestSet(
+            ["-u", "http://localhost:8080", "-t", "123456789", "-f", "csv", "-p", "/home/test/index.csv", "-b", "-1"],
+            null,
+            1,
+            "--blank-lines less than 0"
+        );
+        yield return new CliTestSet(
+            [
+                "-u", "http://localhost:8080", "-t", "123456789", "-f", "csv", "-p", "/home/test/index.csv",
+                "--asn-from", "100", "--asn-to", "50"
+            ],
+            null,
+            1,
+            "--asn-from higher than --asn-to"
+        );
+        yield return new CliTestSet(
+            [
+                "-u", "http://localhost:8080", "-t", "123456789", "-f", "csv", "-p", "/home/test/index.csv", "--asn-to",
+                "-1"
+            ],
+            null,
+            1,
+            "--asn-to less than 0"
+        );
     }
 }
