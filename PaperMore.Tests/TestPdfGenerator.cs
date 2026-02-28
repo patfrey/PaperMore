691553ec6a2165f9677c7084f4c111b876fdbdc2
pfrey <82400804+patfrey@users.noreply.github.com>
2026-02-28 18:04:14 +0100

    add unit tests for PdfGenerator

---
+++ PaperMore.Tests/TestPdfGenerator.cs
@@ -0,0 +1,42 @@
+using System.Security.Cryptography;
+using Microsoft.Extensions.Time.Testing;
+using PaperMore.Reports;
+
+namespace PaperMore.Tests;
+
+[TestFixture]
+public class TestPdfGenerator
+{
+    [Test]
+    public void TestGeneration()
+    {
+        const string expectedDataHash = "6ae37bb83b8694a92c24e239ce2145db9d40c7c37a804864c0fc617044ad0e7c";
+
+        FakeTimeProvider timeProvider = new();
+
+
+        List<DocumentReportData> testData =
+        [
+            new("Reflections on Trusting Trust", 1, "Ken Thompson", DateTimeOffset.Parse("1984-08-01"),
+                DateTimeOffset.Parse("2025-01-01")),
+            new("Hyper Text Coffee Pot Control Protocol", null, "L. Masinter", DateTimeOffset.Parse("1998-04-01"),
+                DateTimeOffset.Parse("2025-01-01")),
+            new("How to share a secret", 2, "Adi Shamir", DateTimeOffset.Parse("1979-11-01"),
+                DateTimeOffset.Parse("2025-01-01")),
+            new(
+                "Dutch courage? Effects of acute alcohol consumption on self-ratings and observer ratings of foreign language skills",
+                3, "Fritz Renner, Inge Kersbergen, Matt Dield, Jessica Werthmann",
+                DateTimeOffset.Parse("2017-10-18"), DateTimeOffset.Parse("2025-01-01"))
+        ];
+
+        using MemoryStream stream = new();
+        PdfGenerator generator = new(timeProvider);
+        generator.Generate(testData, Defaults.DefaultSorting, Defaults.DefaultFilter(null, null, false), stream);
+
+
+        byte[] actualDataHashBytes = SHA256.HashData(stream.ToArray());
+        string actualDataHash = Convert.ToHexStringLower(actualDataHashBytes);
+
+        Assert.That(actualDataHash, Is.EqualTo(expectedDataHash));
+    }
+}
